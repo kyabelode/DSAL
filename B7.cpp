296eec62a905005a7de8709fa587102bbf1093da
kyabelode <absdejk67@gmail.com>
2025-05-05 23:47:13 +0530

    Expression tree
    Signed-off-by: kyabelode <absdejk67@gmail.com>

--- B7.cpp
+++ B7.cpp
@@ -2,65 +2,80 @@
 traverse it using post order traversal (non recursive) and then delete the entire tree */
 #include <bits/stdc++.h>
 using namespace std;
-struct Node{    
-    char key;
-    Node *left,*right;
-    Node(char c ){
-        key = c;
-        left = right = nullptr;
+
+struct Node {
+    char c;
+    Node* left, *right;
+    Node* next; 
+    Node(char v) {
+        c = v;
+        left = nullptr;
+        right = nullptr;
+        next = nullptr;
     }
 };
-void inorder(Node *root){
-    if(root == NULL){
-        return;
+
+class Stack {
+    Node* head;
+
+public:
+    Stack() {
+        head = nullptr;
     }
+
+    void push(Node* root) {
+        root->next = head;
+        head = root;
+    }
+
+    void pop() {
+        if (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+        }
+    }
+
+    Node* top() {
+        return head;
+    }
+
+    bool isEmpty() {
+        return head == nullptr;
+    }
+};
+
+void inorder(Node* root) {
+    if (root == nullptr)
+        return;
     inorder(root->left);
-    cout<<root->key<<"";
+    cout << root->c << " ";
     inorder(root->right);
 }
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+
+int main() {
     string s;
-    cin>>s;
-    Node *x,*y,*z;
-    // we made three nodes for the expression;
-    // consider it as root left right
-    // now make a stack based on the Node structure;
-    stack<Node*>stk;
-    
-    for(int i = s.length() - 1;i>=0;i--){
-        // cause this is the preorder to inorder 
-        // when we will go from first to last there will be exception and tree will not get pushed in the stack
-        // take y as the left node and z as the right node and x as the root node
-        // when +-*/^ will come we have to pop otherwise push becuase its tree we have to consider left and right element 
-        if(s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '^'){
-            // this are the priority wise 
+    cin >> s;  // Example: *+ab-cd
+
+    Stack stk;
+    Node *x, *y, *z;
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+        if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '^') {
             x = new Node(s[i]);
-            // root node to be pushed 
-            // take y as top element 
-            y = stk.top();
-            // pop it 
-            stk.pop();   /* Here we doing pop if its prefix there will be no pop becuase tree is empty cause of this we are travelling from the last end of the array */
-            //take top element as z the right node element 
-            z = stk.top();
-            // we will pop it 
-            stk.pop();
-            // assign left as y;
+            y = stk.top(); stk.pop();
+            z = stk.top(); stk.pop();
             x->left = y;
-            // assign right as x;
             x->right = z;
-            // push it into stack;
             stk.push(x);
-        }   
-        else{
-            // this one is the common condition
+        } else {
             x = new Node(s[i]);
             stk.push(x);
         }
     }
-    cout<<"The expression tree is: \n";
-    inorder(x);
-    cout<<'\n';
+
+    cout << "Inorder traversal of expression tree: ";
+    inorder(stk.top());
+    cout << endl;
+
     return 0;
 }
